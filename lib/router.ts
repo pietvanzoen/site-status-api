91fc31611f71a12ed7a69ed7bfe94e44c4dce1fb
pietvanzoen <hi@pietvanzoen.com>
2021-04-09 17:15:44 +0200

    handle fetch errors

--- lib/router.ts
+++ lib/router.ts
@@ -1,8 +1,11 @@
+import { debug } from "debug/mod.ts";
+
 import { Router, Status } from "oak/mod.ts";
 import { loadConfig } from "./config.ts";
 import { flow } from "./helpers.ts";
 import { buildStatusRequests, StatusRequest } from "./build-statuses.ts";
 
+const log = debug('router');
 const startTime = Date.now();
 const CONFIG_PATH = Deno.env.get("SSAPI_CONFIG") || "./config.yaml";
 
@@ -16,15 +19,21 @@ export function initRouter(): Router {
       buildStatusRequests,
       ({ config, fetchArgs, parseStatus }: StatusRequest) => {
         const { url, options } = fetchArgs;
-        // logFetch(url);
         return fetch(url, options)
+          .then((resp) => {
+            if (resp.ok) return resp;
+            throw resp;
+          })
           .then((r) => r.json())
-          .then((status) => ({
+          .then((status) => {
+            log('%o', status);
+            return {
             ...config,
             currentStatus: parseStatus(status),
-          }))
+          };
+          })
           .catch((error) => {
-            // log(`Error: %s returned an error: %o`, config.name, error);
+            log(`Error: %s returned an error: %o`, config.name, error);
             return {
               ...config,
               currentStatus: "unknown",
